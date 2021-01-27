4c9f56f512652a7dee9d3dc5794c6c61fac1dcf9
goergemiguel <goergemiguel21@gmail.com>
2021-01-27 23:03:17 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bg-green-500 h-screen py-10">
+    <div class="p-12 bg-white">
+      <img alt="Vue logo" src="./assets/logo.png" class="mx-auto" />
+      <HelloWorld msg="Hello Vue 3 + Vite + TailwindCSS" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue"
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
